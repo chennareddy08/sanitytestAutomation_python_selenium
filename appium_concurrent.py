c02c545c0117ce1797e6cff077f16e759f15695b
chennareddy08 <chennareddy.biradar@gmail.com>
2016-03-08 15:43:06 +0530

    Update appium_concurrent.py

--- appium_concurrent.py
+++ appium_concurrent.py
@@ -4,7 +4,7 @@ from booking_simulator import iosbooking
 
 from xmlrunner import xmlrunner
 def appiumserver():
-    os.system("/Applications/Appium.app/Contents/Resources/node/bin/node /Applications/Appium.app/Contents/Resources/node_modules/appium/bin/appium.js --address 127.0.0.1 --chromedriver-port 9516 --bootstrap-port 4725 --no-reset --local-timezone --command-timeout 7200 --session-override --debug-log-spacing  --platform-version 9.0 --platform-name iOS --app Handstand_2.0.7.ipa  --show-ios-log --native-instruments-lib --orientation Portrait")
+    os.system("/Applications/Appium.app/Contents/Resources/node/bin/node /Applications/Appium.app/Contents/Resources/node_modules/appium/bin/appium.js --address 127.0.0.1 --chromedriver-port 9516 --bootstrap-port 4725 --no-reset --local-timezone --command-timeout 7200 --session-override --debug-log-spacing  --platform-version 9.0 --platform-name iOS --app Handstand.app  --show-ios-log --native-instruments-lib --orientation Portrait")
 
 if __name__ == '__main__':
     Process(target=appiumserver).start()
